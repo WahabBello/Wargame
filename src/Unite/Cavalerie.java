c78959bdd276228d4923aa0a7901c679384f21db
ISKO <kofiismael80@gmail.com>
2021-04-21 16:33:24 +0200

    Dossier Unité

---
+++ src/Unite/Cavalerie.java
@@ -0,0 +1,25 @@
+package wargame;
+
+public class Cavalerie extends Unite {
+
+	public Cavalerie(String nom) {
+		super(nom);
+		this.Point_Attaque = 8;
+		this.Point_Defense = 3;
+		this.Point_Deplacement = 8;
+		this.Vision = 6;
+		this.Point_Vie = 38;
+	}
+	
+	public void Niveau_Vie(int degat) {
+		if(degat >=0) {
+			this.Point_vie_restant = this.Point_Vie - degat;
+			System.out.println("Il vous reste " + this.Point_vie_restant + " Points de vie");
+		}
+		else {
+			this.Point_vie_restant = this.Point_Vie + degat;
+			System.out.println("Il vous reste " + this.Point_vie_restant + " Points de vie");
+		}
+	}
+
+}
---
+++ src/Unite/Infanterie.java
@@ -0,0 +1,32 @@
+package wargame;
+
+
+public class Infanterie extends Unite {
+	
+
+	public Infanterie(String nom) {
+		super(nom);
+		this.Point_Attaque = 5;
+		this.Point_Defense = 3;
+		this.Point_Deplacement = 6;
+		this.Vision = 4;
+		this.Point_Vie = 28;
+		
+	}
+
+	public void Niveau_Vie(int degat) {
+		if(degat >=0) {
+			this.Point_vie_restant = this.Point_Vie - degat;
+			System.out.println("Il vous reste " + this.Point_vie_restant + " Points de vie");
+		}
+		else {
+			this.Point_vie_restant = this.Point_Vie + degat;
+			System.out.println("Il vous reste " + this.Point_vie_restant + " Points de vie");
+		}
+	}
+	
+}
+
+
+
+
